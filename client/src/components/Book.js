fd8b6be46cd18f3d1dbc1f8e60ec7b6193055db4
kostalexis89 <kostalexis.hosting@gmail.com>
2021-12-10 15:39:04 +0100

    Books rendering via book component by entering ISBN

---
+++ client/src/components/Book.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Book({title, description, image, author, language}) {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h2>{author}</h2>
+            {description && <p>{description}</p>}
+            
+            <p>{language}</p>
+            {image && <img src={image} alt={title} />}
+            
+
+        </div>
+    )
+}
--- client/src/pages/MyLibrary.js
+++ client/src/pages/MyLibrary.js
@@ -2,26 +2,77 @@ import React, { useState, useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import {AuthContext} from '../context/auth'
-
+import Book from '../components/Book'
 
 export default function MyLibrary() {
     const [isbn, setIsbn] = useState('')
-    const [title, setTitle] = useState('')
-    const [authors, setAuthors] = useState([])
+    const [title, setTitle] = useState(null)
+    const [author, setAuthor] = useState(null)
+    const [description, setDescription] = useState(null)
+    const [image, setImage] = useState(null)
+    const [language, setLanguage] = useState(null)
+    
 
     const handleIsbn = e => setIsbn(e.target.value)
     const handleSubmit = e => {
         e.preventDefault()
         // console.log(isbn)
-        const axiosString = `https://openlibrary.org/isbn/${isbn}.json`
+        const getWorksApi = `https://openlibrary.org/isbn/${isbn}.json`
         // console.log(axiosString)
-        axios.get(axiosString)
+        axios.get(getWorksApi)
         .then(res => {
-            console.log(res.data)
+            //GETTING THE TITLE
+            // console.log(res.data)
+            // console.log("title: " + res.data.title)
             setTitle(res.data.title)
+            // setTitle(res.data.title)
+            const getDataApi = `https://openlibrary.org${res.data.works[0].key}.json`
+            // console.log(getDataApi)
+            axios.get(getDataApi)
+            .then(res=> {
+                console.log(res.data)
+                //FOR AUTHOR
+                const getAuthorApi = `https://openlibrary.org${res.data.authors[0].author.key}.json`
+                axios.get(getAuthorApi)
+                .then(res=> {
+                    // console.log(res.data)
+                     //GETTING THE AUTHOR
+                    // console.log('Author: ' + res.data.name)
+                    setAuthor(res.data.name)
+                })
+                .catch(err => console.log(err))
+                //FOR IMAGE
+                const getImageApi = `https://covers.openlibrary.org/b/id/${res.data.covers[0]}-L.jpg`
+                // console.log(getImageApi)
+                setImage(getImageApi)
+                //There is this response.url. I might need that if i have bugs
+                // axios.get(getImageApi)
+                // .then(res=>{
+                //     console.log(res)
+                // })
+                // .catch(err => console.log(err))
+                //DESCRIPTION
+                setDescription(res.data.description)
+            })
+            .catch(err => console.log(err))
+            //language
+            // console.log(res.data.languages[0].key)
+            const languageApi = `https://openlibrary.org${res.data.languages[0].key}.json`
+            axios.get(languageApi)
+            .then(res => {
+                // console.log(res.data.name)
+                setLanguage(res.data.name)
+            })
         })
+        .catch(err => console.log(err))
     }
+        console.log(title)
+        console.log(description)
+        console.log(author)
+        console.log(language)
+        console.log(image)
         
+
     
     return (
         <div>
@@ -31,6 +82,9 @@ export default function MyLibrary() {
 				<input type="text" name="isbn" value={isbn} onChange={handleIsbn} />
                 <button type="submit">Find Book</button>
 			</form>
+           
+            {title && author && language && <Book title={title} author={author} language={language} description={description} image={image}/>}
+            
         </div>
     )
 }
